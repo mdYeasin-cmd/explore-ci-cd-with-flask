ec369c8f6c02d42a4e85039d923fcd4ecf3e6f2a
mdYeasin-cmd <yeasin200011@gmail.com>
2024-09-12 11:01:05 +0600

    feat: change linter check files

--- flask_starterkit/main/routes/auth.py
+++ flask_starterkit/main/routes/auth.py
@@ -6,6 +6,6 @@ auth_routes = Blueprint("auth_routes", __name__)
 @auth_routes.route('/')
 def welcome():
     return {
-        "message": "         Wrong lint Welcome to your awesome auth endpoint",
-        "success": True
+        "message": "                  Wr     ong         lint Welcome to your awesome auth endpoint",
+        "success": True 
     }
